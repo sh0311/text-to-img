2199abd07ad7096a261a874ee098b65e840e1c8a
sh0311 <0311alice@naver.com>
2023-10-24 13:15:39 +0900

    OUTTA final project

---
+++ test_script.sh
@@ -0,0 +1,2 @@
+python generate_image.py     --prompt "The woman is young and has blond hair, and arched eyebrows."     --load_epoch 4     --checkpoint_path "model_exp1"
+    
---
+++ train_script.sh
@@ -0,0 +1,1 @@
+python main.py     --train_data "train_data_4cap.zip"     --batch_size 10     --num_epochs 5     --learning_rate 0.00017     --report_interval 50     --noise_dim 100     --projection_dim 128     --clip_embedding_dim 512     --checkpoint_path "model_exp3"     --result_path "images_exp3"     --num_stage 3     --resume_epoch -1     --use_uncond_loss --use_contrastive_loss 
